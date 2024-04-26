3246d9960f215548713daa02027c3facb9ca8417
Aviii18 <123736797+Aviii18@users.noreply.github.com>
2024-04-26 14:31:14 +0000

    basics

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId= 144553
+let accountemail= "avi@google.com"
+var accountpassword="12345"
+accountcity="delhi"
+let hh
+
+// accountId= 2 // not allowed
+
+accountemail="abs@gmail.com"
+accountpassword="121212"
+accountcity="agra"
+
+console.log(accountId)
+console.table([accountId,accountemail,accountpassword,accountcity,hh])
+
+/*
+prefer not to use var because of issue in block and functional scope
+*/
